54b6ddfcb1a842b4efd4febdae92287a8a0e3ebf
Cody Nguyen <Thanh.ng13@hotmail.com>
2026-01-22 13:18:24 -0700

    Finished Assignment 2

---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,10 @@
+export default function StudentInfo() {
+  return (
+    <div>
+      <h2>Name: Cody Nguyen</h2>
+      <p>
+        GitHub: <a href="https://github.com/TLN13/CPRG306-Assignments" className="hover:underline">TLN13/CPRG306-Assignments</a>
+      </p>
+    </div>
+  );
+}
